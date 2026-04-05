b9da09ff8a89ffeb732ac6ec21f0114ae173426b
brakdag <brakdag@gmail.com>
2026-04-05 14:18:07 -0300

    chore: remove deprecated and depracated folders

--- depracated/list_models.py
+++
@@ -1,7 +0,0 @@
-from google.generativeai.models import list_models
-from google.generativeai.client import configure
-import os
-
-configure(api_key=os.environ.get("GOOGLE_API_KEY"))
-for m in list_models():
-    print(m.name)
--- depracated/reinstall.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /mnt/DataSD/sandbox/charla/venv
-python3 -m venv /mnt/DataSD/sandbox/charla/venv
-source /mnt/DataSD/sandbox/charla/venv/bin/activate
-pip install google-genai
