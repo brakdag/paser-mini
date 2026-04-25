7a56e42b18d8878a1845375a3ee9643674e8d681
brakdag <brakdag@gmail.com>
2026-04-25 19:21:55 -0300

    achicar god files

---
+++ src/core/command_handlers/system.py
@@ -0,0 +1,12 @@
+import os
+
+class SystemCommands:
+    @staticmethod
+    def handle_clear(ui):
+        os.system("cls" if os.name == "nt" else "clear")
+        return True
+
+    @staticmethod
+    def handle_exit(chat_manager):
+        chat_manager.should_exit = True
+        return True
