e52246b5fba8a8454e8dc5e5bef84a891090383e
brakdag <brakdag@gmail.com>
2026-05-09 16:02:24 -0300

    directory solution

--- src/core/configManager.js
+++ src/core/configManager.js
@@ -1,9 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export class ConfigManager {
   constructor() {
-    this.configPath = path.join(process.cwd(), 'config', 'config.json');
+    this.configPath = path.join(__dirname, '..', '..', 'config', 'config.json');
     this.config = this._loadConfig();
   }
 
