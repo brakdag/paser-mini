c525e0786f59ba9aa59deb5e9d678b3e00b5af19
brakdag <brakdag@gmail.com>
2026-05-16 12:20:02 -0300

    reparando con opencode terminado

--- src/core/schemaRegistry.js
+++ src/core/schemaRegistry.js
@@ -22,7 +22,7 @@ async function registerSchemas() {
         try {
           // eslint-disable-next-line no-await-in-loop
           const module = await import(`./schemas/${file}?update=${Date.now()}`);
-          const schema = module[`${schemaName}Schema`];
+          let schema = module.default || module[`${schemaName}Schema`];
 
           if (schema) {
             validator.registerSchema(toolName, schema);
